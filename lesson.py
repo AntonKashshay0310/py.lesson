b2ce6270f81cce369e9df436d0df8168216628f0
AntonKashshay0310 <antosha.kashshay@gmail.com>
2025-12-27 22:18:30 +0200

    Create lesson.py

---
+++ lesson.py
@@ -0,0 +1,32 @@
+import turtle
+
+raphael = turtle.Turtle()
+wn = turtle.Screen()
+
+
+wn.title("Raphael")
+wn.bgcolor("red")
+
+
+raphael.forward(120)
+raphael.left(60)
+
+raphael.forward(80)
+raphael.left(120)
+
+raphael.forward(120)
+raphael.left(60)
+
+raphael.forward(80)
+raphael.left(120)
+
+
+
+
+
+
+
+
+wn.mainloop()
+
+turtle.exitonclick()
